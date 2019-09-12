951f408021655d8514f0737ce3027beb0bd9af05
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2019-09-12 18:33:06 -0400

    Changed @PostMapping

--- src/main/java/com/kajoyrest/test/CursoController.java
+++ src/main/java/com/kajoyrest/test/CursoController.java
@@ -1,9 +1,12 @@
 package com.kajoyrest.test;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,8 +46,13 @@ class CursoController {
     }
 
     @PostMapping("/cursos")
-    Curso newCurso(@RequestBody Curso newCurso) {
-        return repository.save(newCurso);
+    ResponseEntity<?> newEmployee(@RequestBody Curso newCurso) throws URISyntaxException {
+
+        Resource<Curso> resource = assembler.toResource(repository.save(newCurso));
+
+        return ResponseEntity
+                .created(new URI(resource.getId().expand().getHref()))
+                .body(resource);
     }
 
     @GetMapping("/cursos/{id}")
