caa3bd55934b1f5dfb34de4eb1a48e31771ae169
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2019-09-12 18:29:29 -0400

    Added Curso record that handles both old and new clients

--- src/main/java/com/kajoyrest/test/Curso.java
+++ src/main/java/com/kajoyrest/test/Curso.java
@@ -14,11 +14,23 @@ class Curso {
     private @Id @GeneratedValue Long id;
     private String grado;
     private String paralelo;
+    private String bloque;
 
     Curso() { }
 
-    public Curso(String grado, String paralelo) {
+    public Curso(String grado, String paralelo, String bloque) {
         this.grado = grado;
         this.paralelo = paralelo;
+        this.bloque = bloque;
+    }
+
+    public String getNivel() {
+        return  this.grado+" "+this.paralelo;
+    }
+
+    public void setNivel(String nivel){
+        String[] parts = nivel.split(" ");
+        this.grado = parts[0];
+        this.paralelo = parts[1];
     }
 }
