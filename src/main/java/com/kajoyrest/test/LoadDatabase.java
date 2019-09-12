52730e18bf38ac095a4eb7260a2ace9437484ca4
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2019-09-12 16:42:57 -0400

    added loaddatabase.java

---
+++ src/main/java/com/kajoyrest/test/LoadDatabase.java
@@ -0,0 +1,19 @@
+package com.kajoyrest.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+    @Bean
+    CommandLineRunner initDatabase(CursoRepository repository) {
+        return args -> {
+            log.info("Precargando " + repository.save(new Curso("5to Secundaria", "5B")));
+            log.info("Precargando " + repository.save(new Curso("3ro Primaria", "3A")));
+        };
+    }
+}
