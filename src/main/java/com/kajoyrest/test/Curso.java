cad10bc54153a3e64ce54d44b2c43db7e41711d8
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2019-09-12 15:47:24 -0400

    added curso.java

---
+++ src/main/java/com/kajoyrest/test/Curso.java
@@ -0,0 +1,24 @@
+package com.kajoyrest.test;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+class Curso {
+
+    private @Id @GeneratedValue Long id;
+    private String grado;
+    private String paralelo;
+
+    Curso() { }
+
+    public Curso(String grado, String paralelo) {
+        this.grado = grado;
+        this.paralelo = paralelo;
+    }
+}
